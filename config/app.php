4bedcf66928b263584a49f1a5359af24e9a164c6
ubuntu <ubuntu@ubuntu-All-Series.DHCP>
2016-04-09 23:04:39 +0800

    config on server

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -79,7 +79,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'F6KNncpeMlsQew8kfceXP8PYDpzbXvkO'),
 
     'cipher' => 'AES-256-CBC',
 
--- public/extract_item.py
+++ public/extract_item.py
@@ -1,5 +1,13 @@
 import os,sys
 import csv
+import locale
+
+def sort_tuple_by_pinyin(datalist):
+    return sorted(datalist,cmp = tuple_cmp)
+
+def tuple_cmp(x,y):
+    locale.setlocale(locale.LC_COLLATE, 'zh_CN@pinyin.utf8')
+    return locale.strcoll(x[1],y[1])
 
 def usage():
 	print '-Usage:'
@@ -19,19 +27,29 @@ else:
 
 	dirs = os.listdir(itemDir)
 
-	with open(outputFilePath, 'w') as csvfile:
-		fieldnames = ['slug','path']
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-		for dir in dirs:
-			if dir.startswith('.'):
-				continue
-			dirpath = os.path.join(itemDir,dir)
-			if os.path.isdir(dirpath):
-				files = os.listdir(dirpath)
+	flists = []
+	for dir in dirs:
+		if dir.startswith('.'):
+			continue
+		dirpath = os.path.join(itemDir,dir)
+		if os.path.isdir(dirpath):
+			splitdir = os.listdir(dirpath)
+			for splitd in splitdir:
+				if splitd.startswith('.'):
+					continue
+				obj_path = os.path.join(dirpath,splitd)
+				files = 	os.listdir(obj_path)
 				for file in files:
 					if file.startswith('.'):
 						continue
-					path = os.path.join(dirpath,file)
-					slug = dir
-					print slug,path
-					writer.writerow({'slug': slug, 'path':path})
+					path = os.path.join(obj_path,file)
+					slug = splitd
+					flists.append((slug,path))
+
+	with open(outputFilePath, 'w') as csvfile:
+		fieldnames = ['slug','path']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		flists  = sort_tuple_by_pinyin(flists)
+		for slug,path in flists:
+			print slug,path
+			writer.writerow({'slug': slug, 'path':path})
